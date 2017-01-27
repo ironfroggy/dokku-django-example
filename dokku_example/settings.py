ce33bb9178f1aabcf0a16796bb900e95e19982c2
Calvin Spealman <calvin@caktusgroup.com>
2017-01-26 22:21:55 -0500

    .

--- dokku_example/settings.py
+++ dokku_example/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # https://pypi.python.org/pypi/django-celery
